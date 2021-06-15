00cc5131bc0e8bbee5db270ace90861c8fc2ef14
Pedro Henrique da Paixão  Carneiro <pedrohenriquedapaixao@outlook.com>
2021-06-15 13:07:23 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="bg-todo">
+    </header>
+    <div class="container form-todo">
+      <form class="" @submit.prevent="addTodo(todo)">
+        <h1 class="titulo">To do</h1>
+        <div class="input-group">
+          <input type="text" class="todo-input" v-model="todo.description"  name="todo" placeholder="Novo To-Do">
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+        
+      </form>
+      
+      <div class="todo-wrapper">
+        <Todo v-for="(t,index) in todos" :key="t.id" :todo = "t"  v-on:done="makeDone(index)" v-on:del="del(index)"/>
+      </div>
+
+    </div>   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todo from './components/Todo';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Todo },
+  data(){
+    return {todos: [],todo: {checked: false}}
+  },
+  methods: {
+      addTodo(todo){
+        todo.id = Date.now()
+        this.todos.push(todo)
+        this.todo = {checked: false}
+      },
+      makeDone(index){
+        this.todos[index].checked = !this.todos[index].checked;
+      },
+      del(index){
+        this.todos.splice(index,1)
+      }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .bg-todo{
+    background-image: linear-gradient(to right,rgba(149, 67, 217,.8),rgba(110, 164, 251,.8)), url("/img/wallpaper.jpg");
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 40vh;
+  }
+
+  .titulo{
+    text-transform: uppercase;
+    color: #F4F4F4;
+    font-size: 80px;
+    text-align: left;
+  }
+
+  .btn{
+    background-color: #41B883;
+    width: 150px;
+    padding: 20px 0;
+    border: 0;
+    border-radius: 0px 5px 5px 0px;
+    text-transform: uppercase;
+    color: #F3F3F3;
+    font-weight: bold;
+    cursor: pointer;
+    font-size: 16px;
+    transition: all .3s;
+  }
+
+  .btn:hover{
+    background-color: #41AA83;
+  }
+
+  .form-todo{
+    z-index: 100;
+    transform: translateY(-250px);
+  }
+
+  .todo-input{
+    width: calc(100% - 150px);
+    padding: 20px 10px;
+    border: 0;
+    border-radius: 5px 0px 0px 5px;
+    font-weight: bold;
+    margin: 40px 0px;
+    font-size: 16px;
+  }
+  .todo-input:focus,.todo-input:active{
+    outline: none;
+  }
+
+  .todo-wrapper{
+    background-color: #FAFAFA;
+    box-shadow: 0px 10px 25px rgba(0,0,0,.2);
+    border-radius: 5px; 
+    overflow: hidden;
+  }
+  
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,75 @@
+<template>
+  <ul class="list-todo" v-if="todo">
+    <li class="list-todo__item" v-bind:class="{ risk: todo.checked }">
+      <p class="list-todo__content" v-bind:class="{ riskline: todo.checked }">{{ todo.description }}</p>
+      <div class="list-todo__botoes">
+        <button class="btn-done" v-bind:class="{ btndone: todo.checked }" @click="$emit('done')">Concluir</button>
+        <button class="btn-delete"  @click="$emit('del')">Delete</button>
+      </div>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props:{
+    todo:{ type: Object, required: true}
+  },
+}
+</script>
+<style scoped>
+  .list-todo{
+    width: 100%;
+    list-style: none;
+  }
+
+  .list-todo__item{
+    color: #181818;
+    cursor: pointer;
+    padding: 20px 10px;
+    display: flex;
+    font-weight: bold;
+    font-size: 16px;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .list-todo__content{
+    width: 80%;
+    text-align: left;
+  }
+  .list-todo__botoes{
+    width: 30%;
+    text-decoration: none;
+    display: flex;
+  }
+
+  button{
+    background-color: none;
+    border: none;
+    width: 350px;
+    height: 50px;
+    border-radius: 5px;
+    cursor:pointer;
+    margin-right: 3px;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #181818;
+  }
+  
+  .btn-delete{
+    background-color: #FF0000;
+  }
+  .btn-delete:active{
+    background-color: #AA0000;
+  }
+
+  .btndone{
+    border: 1px solid #181818;
+  }
+  .riskline{
+    text-decoration: line-through; 
+  }
+  .risk{
+    background-color: #c1c1c1;
+  }
+</style>
